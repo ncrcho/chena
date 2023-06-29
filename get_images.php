1a28a68102fd36c8e612a96b09451632bc962ca4
ncrcho <110095014+ncrcho@users.noreply.github.com>
2023-06-29 09:21:04 +0100

    edit

---
+++ get_images.php
@@ -0,0 +1,23 @@
+<?php
+$portfolioDir = './portfolio/';
+$portfolioHoverDir = './portfolio_hover/';
+
+$images = [];
+
+// Retrieve image files from portfolio directory
+$portfolioFiles = scandir($portfolioDir);
+foreach ($portfolioFiles as $file) {
+  if ($file !== '.' && $file !== '..') {
+    $imagePath = $portfolioDir . $file;
+    $hoverImagePath = $portfolioHoverDir . $file;
+    $images[] = [
+      'src' => $imagePath,
+      'alt' => 'Image',
+      'hoverSrc' => $hoverImagePath,
+    ];
+  }
+}
+
+// Convert the images array to JSON and send the response
+header('Content-Type: application/json');
+echo json_encode($images);
