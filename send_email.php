44c4bc9d7d0f8f00d2c34c51f33169f7934ea999
ncrcho <110095014+ncrcho@users.noreply.github.com>
2023-07-01 08:31:03 +0100

    y

--- send_email.php
+++ send_email.php
@@ -24,12 +24,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $body .= "Message: " . $message;
     $headers = "From: " . $email;
 
-    $result = mail($to, $subject, $body, $headers);
-    echo $result;
+    // Email configuration
+    $mailConfig = array(
+        'protocol' => 'imap',
+        'host' => 'imap.barid.com',
+        'port' => 993,
+        'username' => 'fsd@barid.com',
+        'password' => '@C4cYc6g9XHyw_v', // Replace with the actual password
+        'encryption' => 'ssl',
+        'smtpHost' => 'smtp.barid.com',
+        'smtpPort' => 465,
+        'smtpUsername' => 'fsd@barid.com',
+        'smtpPassword' => '@C4cYc6g9XHyw_v', // Replace with the actual password
+    );
+
+    // Send email using the configured mail settings
+    $result = sendMail($mailConfig, $to, $subject, $body, $headers);
+
     if ($result) {
         echo "success";
     } else {
         echo 'error: Failed to send email';
     }
+}
+
+function sendMail($mailConfig, $to, $subject, $body, $headers) {
+    // Create an IMAP connection
+    $imap = imap_open(
+        '{' . $mailConfig['host'] . ':' . $mailConfig['port'] . '/imap/' . $mailConfig['encryption'] . '}',
+        $mailConfig['username'],
+        $mailConfig['password']
+    );
+
+    if (!$imap) {
+        return false;
+    }
+
+    // Compose the email
+    $message = "To: " . $to . "\r\n";
+    $message .= "Subject: " . $subject . "\r\n";
+    $message .= $headers . "\r\n\r\n";
+    $message .= $body;
+
+    // Send the email
+    $sent = imap_mail($to, $subject, $message, $headers);
+
+    // Close the IMAP connection
+    imap_close($imap);
+
+    return $sent;
 }
 ?>
