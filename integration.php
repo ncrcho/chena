fe8144e12d919a61cc89840211f359bbb4b50e97
ncrcho <110095014+ncrcho@users.noreply.github.com>
2023-07-01 07:21:57 +0100

    ee

--- integration.php
+++ integration.php
@@ -1,79 +1,84 @@
 <?php
 
-// Retrieve the payload data sent by GitHub
-$payload = file_get_contents('php://input');
-$data = json_decode($payload, true);
+$directory = '/www/chena.pro/htdocs';
 
-// Verify the payload and its authenticity (optional but recommended)
-$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
-$secret = 'your_webhook_secret'; // Replace with your actual secret
-$isPayloadValid = verifyPayloadSignature($payload, $signature, $secret);
+exec("cd $directory && git pull ");
 
-if ($isPayloadValid) {
-    // Process the webhook event based on its type
-    $event = $_SERVER['HTTP_X_GITHUB_EVENT'] ?? '';
 
-    switch ($event) {
-        case 'push':
-            // Handle the push event
-            processPushEvent($data);
-            break;
-        case 'pull_request':
-            // Handle the pull request event
-            processPullRequestEvent($data);
-            break;
-        // Add other event types as needed
-        default:
-            // Unsupported event type
-            http_response_code(400);
-            exit('Unsupported event type');
-    }
+// // Retrieve the payload data sent by GitHub
+// $payload = file_get_contents('php://input');
+// $data = json_decode($payload, true);
 
-    // Respond with a success status
-    http_response_code(200);
-    exit('Webhook event processed successfully');
-} else {
-    // Respond with an error status
-    http_response_code(401);
-    exit('Invalid payload signature');
-}
+// // Verify the payload and its authenticity (optional but recommended)
+// $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'] ?? '';
+// $secret = 'your_webhook_secret'; // Replace with your actual secret
+// $isPayloadValid = verifyPayloadSignature($payload, $signature, $secret);
 
-// Function to verify the authenticity of the payload
-function verifyPayloadSignature($payload, $signature, $secret) {
-    list($algo, $hash) = explode('=', $signature, 2);
-    $payloadHash = hash_hmac($algo, $payload, $secret);
+// if ($isPayloadValid) {
+//     // Process the webhook event based on its type
+//     $event = $_SERVER['HTTP_X_GITHUB_EVENT'] ?? '';
 
-    return $hash === $payloadHash;
-}
+//     switch ($event) {
+//         case 'push':
+//             // Handle the push event
+//             processPushEvent($data);
+//             break;
+//         case 'pull_request':
+//             // Handle the pull request event
+//             processPullRequestEvent($data);
+//             break;
+//         // Add other event types as needed
+//         default:
+//             // Unsupported event type
+//             http_response_code(400);
+//             exit('Unsupported event type');
+//     }
 
-// Function to handle the push event
-function processPushEvent($data) {
-    // Perform actions based on the push event
-    // For example, pull the latest code, run build processes, etc.
-    // You can use system commands or execute custom PHP code here
+//     // Respond with a success status
+//     http_response_code(200);
+//     exit('Webhook event processed successfully');
+// } else {
+//     // Respond with an error status
+//     http_response_code(401);
+//     exit('Invalid payload signature');
+// }
 
-    // Replace `/www/chena.pro/htdocs` with your actual directory path
-    $directory = '/www/chena.pro/htdocs';
+// // Function to verify the authenticity of the payload
+// function verifyPayloadSignature($payload, $signature, $secret) {
+//     list($algo, $hash) = explode('=', $signature, 2);
+//     $payloadHash = hash_hmac($algo, $payload, $secret);
 
-    // Change the ownership of the directory to the web server user
-    exec("sudo chown -R www-data:www-data $directory");
+//     return $hash === $payloadHash;
+// }
 
-    // Execute `git pull` with `sudo` privileges
-    exec("sudo -u www-data git -C $directory pull");
-}
+// // Function to handle the push event
+// function processPushEvent($data) {
+//     // Perform actions based on the push event
+//     // For example, pull the latest code, run build processes, etc.
+//     // You can use system commands or execute custom PHP code here
 
-// Function to handle the pull request event
-function processPullRequestEvent($data) {
-    // Perform actions based on the pull request event
-    // For example, validate the pull request, run tests, etc.
-    // You can use system commands or execute custom PHP code here
+//     // Replace `/www/chena.pro/htdocs` with your actual directory path
+//     $directory = '/www/chena.pro/htdocs';
 
-    // Replace `/www/chena.pro/htdocs` with your actual directory path
-    $directory = '/www/chena.pro/htdocs';
+//     // Change the ownership of the directory to the web server user
+//     exec("sudo chown -R www-data:www-data $directory");
 
-    // Change the ownership of the directory to the web server user
-    exec("chown -R www-data:www-data $directory");
+//     // Execute `git pull` with `sudo` privileges
+//     exec("sudo -u www-data git -C $directory pull");
+// }
 
-    // Execute `git pull` with `sudo` privileges
-    exec("-u www-data git -C $directory pull");
-}
+// // Function to handle the pull request event
+// function processPullRequestEvent($data) {
+//     // Perform actions based on the pull request event
+//     // For example, validate the pull request, run tests, etc.
+//     // You can use system commands or execute custom PHP code here
+
+//     // Replace `/www/chena.pro/htdocs` with your actual directory path
+//     $directory = '/www/chena.pro/htdocs';
+
+//     // Change the ownership of the directory to the web server user
+//     exec("chown -R www-data:www-data $directory");
+
+//     // Execute `git pull` with `sudo` privileges
+//     exec("-u www-data git -C $directory pull");
+// }
