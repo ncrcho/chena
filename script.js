3bd81d3e25168bad75ad47f06ece181e213dcd1a
ncrcho <110095014+ncrcho@users.noreply.github.com>
2023-08-25 11:48:57 +0100

    v2

--- script.js
+++ script.js
@@ -1,120 +1,191 @@
-let currentPage = 1; // Define the initial current page
-
-// Function to generate the image elements for a specific page
-// Function to generate the image elements for a specific page
-function generateImageElements(images, page, imagesPerPage) {
-  const gridImages = document.getElementById('gridImages');
-  gridImages.innerHTML = ''; // Clear previous images
-
-  const startIndex = (page - 1) * imagesPerPage;
-  const endIndex = startIndex + imagesPerPage;
-  const pageImages = images.slice(startIndex, endIndex);
-
-  for (let i = 0; i < pageImages.length; i++) {
-    const imageObj = pageImages[i];
-    const image = document.createElement('img');  //---------*******************************--
-    image.classList.add('portfolio-image');
-
-    image.src = imageObj.src;
-    image.alt = imageObj.alt;
-    image.dataset.originalSrc = imageObj.src; // Store original source
-    image.dataset.hoverSrc = imageObj.hoverSrc;
-    
-    // Add event listeners for hover functionality
-    image.addEventListener('mouseover', function() {
-      image.src = image.dataset.hoverSrc; // Change source on hover
-    });
-
-    image.addEventListener('mouseout', function() {
-      image.src = image.dataset.originalSrc; // Restore original source on mouseout
-    });
-
-    gridImages.appendChild(image);
 
 
 
-    
-  }
-}
+window.addEventListener("scroll", function () {
+  const section2 = document.getElementById('section_3');
+
+  const title = section2.querySelector('#title');
+  const text = section2.querySelector('#text');
 
 
-function generatePagination(images, imagesPerPage) {
-  const totalPages = Math.ceil(images.length / imagesPerPage);
-  const pagination = document.getElementById('pagination');
-  pagination.innerHTML = ''; // Clear previous pagination
+  const section2Top = section2.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
 
-  // Previous button
-  const previousButton = document.createElement('button');
-  previousButton.innerHTML = '<img src="./icon_preview.svg" alt="Previous">';
-  previousButton.classList.add('page-link', 'previous-button', 'hide');
-  previousButton.disabled = currentPage === 1;
-  previousButton.addEventListener('click', function () {
-    currentPage--; // Update current page
-    generatePagination(images, imagesPerPage);
-    generateImageElements(images, currentPage, imagesPerPage);
-  });
-  pagination.appendChild(previousButton);
+  if (section2Top < screenHeight && section2Top > 0) {
+    // section2.classList.add("translation-animation");
 
-  for (let i = 1; i <= totalPages; i++) {
-    const button = document.createElement('button');
-    button.textContent = i;
-    button.classList.add('page-link');
-    if (i === currentPage) {
-      button.classList.add('active'); // Add 'active' class to current page button
-      button.style.fontWeight = 'bold'; // Apply bold font style to current page button
-      button.style.color = '#E8684D'; // Apply orange color to current page button
-    }
-    button.addEventListener('click', function () {
-      currentPage = i; // Update current page
-      generatePagination(images, imagesPerPage);
-      generateImageElements(images, currentPage, imagesPerPage);
-    });
-    pagination.appendChild(button);
+    title.classList.add("translation-animation");
+    text.classList.add("translation-animation-down");
   }
 
-  // Next button
-  const nextButton = document.createElement('button');
-  nextButton.innerHTML = '<img src="./icon_next.svg" alt="Next">';
-  nextButton.classList.add('page-link', 'next-button');
-  nextButton.disabled = currentPage === totalPages;
-  nextButton.addEventListener('click', function () {
-    currentPage++; // Update current page
-    generatePagination(images, imagesPerPage);
-    generateImageElements(images, currentPage, imagesPerPage);
-  });
-  pagination.appendChild(nextButton);
 
-  // Hide previous button on first page
-  if (currentPage === 1) {
-    previousButton.classList.add('hide');
-  }
+  const boxes = document.querySelectorAll('.box'); // Select all boxes
+  // const screenHeight = window.innerHeight;
 
-  // Hide next button on last page
-  if (currentPage === totalPages) {
-    nextButton.classList.add('hide');
-  }
-}
+  boxes.forEach((box, index) => {
+    const boxTop = box.getBoundingClientRect().top;
 
-
-
-// Retrieve image data from PHP script
-function retrieveImageData() {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'get_images.php', true);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        const images = JSON.parse(xhr.responseText);
-        const imagesPerPage = 9; // Define the images per page
-        generatePagination(images, imagesPerPage);
-        generateImageElements(images, currentPage, imagesPerPage);
+    if (boxTop < screenHeight && boxTop > 0) {
+      if (index < 3) {
+        box.classList.add("box-animation");
       } else {
-        console.error('Error: ' + xhr.status);
+        box.classList.add("box-animation");
       }
     }
-  };
-  xhr.send();
+  });
+
+
+
+  const portfolio_images = document.querySelectorAll('.portfolio-image'); // Select all boxes
+  // const screenHeight = window.innerHeight;
+
+  portfolio_images.forEach((box, index) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < screenHeight && boxTop > 0) {
+      if (index < 3) {
+        box.classList.add("img-animation");
+      } else {
+        box.classList.add("img-animation");
+      }
+    }
+  });
+
+
+
+
+  const portfolio_title = document.querySelector('.portfolio_title');
+
+
+  const portfolio_titleTop = portfolio_title.getBoundingClientRect().top;
+
+  if (portfolio_titleTop < screenHeight && portfolio_titleTop > 0) {
+    // section2.classList.add("translation-animation");
+
+    portfolio_title.classList.add("portfolio-title-animation");
+  }
+
+  
+
+
+  const section4 = document.getElementById('section_4');
+  const section4Top = section4.getBoundingClientRect().top;
+
+  if (section4Top < screenHeight && section4Top > 0) {
+    // section2.classList.add("translation-animation");
+
+    section4.classList.add("box-animation");
+  }
+
+
+  const section5 = document.getElementById('section_5');
+  const h5 = section5.querySelector("h5");
+  const p = section5.querySelector("p");
+  const button = section5.querySelector("button");
+
+
+  const section5Top = section5.getBoundingClientRect().top;
+
+  if (section5Top < screenHeight && section5Top > 0) {
+    // section2.classList.add("translation-animation");
+
+    h5.classList.add("footer-sub-title-animation");
+    p.classList.add("footer-title-animation");
+    button.classList.add("footer-sous-title-animation");
+  }
+
+
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopButton = document.getElementById("scrollToTop");
+  const section1 = document.querySelector("header");
+
+  scrollToTopButton.addEventListener("click", function () {
+    const section1Top = section1.getBoundingClientRect().top;
+    const offset = section1Top + window.scrollY;
+    
+    window.scrollTo({
+      top: offset,
+      behavior: "smooth"
+    });
+  });
+
+
+
+
+});
+
+
+function scrollToSection(sectionId) {
+  const section = document.querySelector(sectionId);
+
+  if (section) {
+    const sectionTop = section.getBoundingClientRect().top;
+    const offset = sectionTop + window.scrollY;
+    
+    window.scrollTo({
+      top: offset,
+      behavior: "smooth"
+    });
+  }
 }
 
-// Call the function to retrieve image data
-retrieveImageData();
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollToTopButton = document.getElementById("scrollToTop");
+  const work = document.getElementById("work");
+  const about = document.getElementById("about");
+  const service = document.getElementById("service");
+
+
+  
+  scrollToTopButton.addEventListener("click", function () {
+    scrollToSection("#header");
+  });
+
+  work.addEventListener("click", function () {
+    scrollToSection(".portfolio");
+  });
+  about.addEventListener("click", function () {
+    scrollToSection("#section_4");
+  });
+
+  service.addEventListener("click", function () {
+    scrollToSection("#section99");
+  });
+
+
+
+
+  const work_mob = document.getElementById("work_mob");
+  const about_mob = document.getElementById("about_mob");
+  const service_mob = document.getElementById("service_mob");
+
+
+  
+  scrollToTopButton.addEventListener("click", function () {
+    scrollToSection("#header");
+  });
+
+  work_mob.addEventListener("click", function () {
+    scrollToSection(".portfolio");
+  });
+  about_mob.addEventListener("click", function () {
+    scrollToSection("#section_4");
+  });
+
+  service_mob.addEventListener("click", function () {
+    scrollToSection("#section99");
+  });
+
+
+
+
+  
+
+
+
+
+});
+
